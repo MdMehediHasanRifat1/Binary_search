13fcca547953152bad96f4db83362d815fd75957
MdMehediHasanRifat1 <rifatahammed2222@gmail.com>
2025-01-30 23:43:46 +0600

    UVA 11413

---
+++ Fill the Containers.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+ll a[1000009];
+ll n,m,i,j;
+bool isok(ll x)
+{
+	ll sum=0;
+	ll cnt=1;
+	bool f=1;
+	for(i=0;i<n;i++)
+	{
+		if(sum+a[i]<=x)
+		{
+			sum+=a[i];
+		}
+		else
+		{
+			if(a[i]>x)
+			{
+				f=0;
+				break;
+			}
+			sum=a[i];
+			cnt++;
+		}
+	}
+	if(cnt<=m&&f)
+		return true;
+	else
+		return false;
+}
+int main()
+{
+	while(scanf("%lld%lld",&n,&m)!=EOF)
+	{
+		ll ans=0;
+		for(i=0;i<n;i++)
+			cin>>a[i];
+		ll l=0,h=1e10,mid=0;
+		while(l<=h)
+		{
+			mid=(l+h)/2;
+			if(isok(mid))
+			{
+				ans=mid;
+				h=mid-1;
+			}
+			else
+			{
+				l=mid+1;
+			}
+		}
+		printf("%lld\n",ans);
+	}
+	return 0;
+}
